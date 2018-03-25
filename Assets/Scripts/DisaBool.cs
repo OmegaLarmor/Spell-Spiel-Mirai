059f40680c1c854f77c50c846b3e74289fa85e39
OmegaLarmor <remy-lapointe@hotmail.ca>
2018-03-24 21:23:55 -0400

    BooleanVariable can be used to enable/disable UI elements.

---
+++ Assets/Scripts/DisaBool.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisaBool : MonoBehaviour
+{
+
+    public BooleanVariable boolean;
+    private bool previousValue;
+
+    public bool invert = false;
+
+	public GameObject[] toDisable;
+
+	bool toSet;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (invert == false)
+        {
+            toSet = boolean.value;
+        }
+
+		else if (invert){
+			
+            toSet = !boolean.value;
+		}
+
+		
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (invert == false)
+        {
+            if (previousValue != boolean.value)
+            {
+				toSet = boolean.value;
+            }
+        }
+
+        else if (invert)
+        {
+            if (previousValue == boolean.value)
+            {
+				toSet = !boolean.value;
+            }
+        }
+
+		ChangeAllChildren(toSet);
+		previousValue = toSet;
+    }
+
+	private void ChangeAllChildren(bool toSet){
+		for (int i = 0; i < toDisable.Length; i++){
+			toDisable[i].SetActive(toSet);
+		}
+	}
+}
